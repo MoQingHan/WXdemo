e4ea03ced983c285c0b8c1ce9d48ee5e6dc32a2d
MoQingHan <42321921+MoQingHan@users.noreply.github.com>
2019-01-23 16:05:38 +0800

    feat:log4j2的日志配置，最后注释掉了，打算采用logback

---
+++ src/test/java/com/testspringboot/lottery/service/DemoServiceTest.java
@@ -0,0 +1,46 @@
+package com.testspringboot.lottery.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoServiceTest {
+    @Autowired
+    private DemoService demoService;
+
+    @Test
+    public void getUserByOpenid() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void addUser1() {
+    }
+
+    @Test
+    public void bet() {
+    }
+
+    @Test
+    public void updateUserInfo() {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void addMoney() {
+    }
+
+    @Test
+    public void selectAllWxUser() {
+    }
+}
